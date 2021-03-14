26b51de3469d43876754bdef9c134a0873856258
Murat Demirtas <muratdemirtas2001@gmail.com>
2021-03-14 22:01:14 +0000

    Adding searchbar

--- script.js
+++ script.js
@@ -1,12 +1,80 @@
 //You can edit ALL of the code here
+const rootElem = document.getElementById("root");
+let episodeContainer = document.createElement("div");
+addSearchBar();
+
+rootElem.appendChild(episodeContainer);
+
+const allEpisodes = getAllEpisodes();
+
 function setup() {
-  const allEpisodes = getAllEpisodes();
+  // addSearchbarEvent();
   makePageForEpisodes(allEpisodes);
 }
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+function addSearchBar() {
+  const searchInput = document.createElement("input");
+  rootElem.appendChild(searchInput);
+  searchInput.classList.add("search-input");
+  const searchTotal = document.createElement("p");
+  rootElem.appendChild(searchTotal);
+  searchInput.addEventListener("input", (e) => {
+    let searchInputValue = e.target.value;
+    // console.log(searchInputValue);
+
+    let episodeList = allEpisodes.filter((episode) => {
+      // console.log(episode.name.includes("Winter"));
+      console.log(searchInputValue);
+      let newName = episode.name.toUpperCase();
+      let newSummary = episode.summary.toUpperCase();
+      let newSearchInputValue = searchInputValue.toUpperCase();
+      return (
+        newName.includes(newSearchInputValue) ||
+        newSummary.includes(newSearchInputValue)
+      );
+    });
+    episodeContainer.innerHTML = ``;
+    searchTotal.innerText="Total episode found:"+episodeList.length
+    if (episodeList.length === 0) {
+      episodeContainer.innerText = "Sorry, please search again";
+    } else {
+      makePageForEpisodes(episodeList);
+    }
+  });
 }
 
+function makePageForEpisodes(episodeList) {
+  const episodeSection = document.createElement("section");
+  episodeContainer.appendChild(episodeSection);
+  episodeSection.classList.add("episode-section");
+  console.log(episodeList);
+  episodeList.forEach((episode) => {
+    let episodeDiv = document.createElement("div");
+    episodeSection.appendChild(episodeDiv);
+    episodeDiv.classList.add("episode-div");
+
+    let episodeNameParagraph = document.createElement("p");
+    episodeDiv.appendChild(episodeNameParagraph);
+    if (episode.season < 10) {
+      episode.season = `0${episode.season}`;
+    }
+    if (episode.number < 10) {
+      episode.number = `0${episode.number}`;
+    }
+    episodeNameParagraph.innerText = `${episode.name}-S${episode.season}E${episode.number}`;
+    episodeNameParagraph.classList.add("episodeName-paragraph");
+
+    let episodeImage = document.createElement("img");
+    episodeImage.src = episode.image.original;
+    episodeDiv.appendChild(episodeImage);
+    episodeImage.classList.add("episode-image");
+
+    let episodeSummary = document.createElement("p");
+    episodeDiv.appendChild(episodeSummary);
+    episodeSummary.innerText = episode.summary;
+    episodeSummary.classList.add("episodeSummary-paragraph");
+  });
+
+  // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+}
 window.onload = setup;
